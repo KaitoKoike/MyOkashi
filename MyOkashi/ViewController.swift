c5d910175c53a303d2ce11a94bfe653b13751c5a
KaitoKoike <kaikai.poteti@keio.jp>
2020-03-06 01:56:53 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by 小池開人 on 2020/03/06.
+//  Copyright © 2020 KKid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
